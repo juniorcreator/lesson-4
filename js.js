311f68a16acd9175cce3786f76a55d58551f4828
serhii <kovalenkoserg1990@gmail.com>
2019-10-28 22:55:57 +0200

    dwd

--- js.js
+++ js.js
@@ -1,1 +1,15 @@
-console.log('wdwedew');
+function Human(name = 'Vova', age = 30) {
+  this.name = name;
+  this.age = age;
+  let eat = 0;
+  let drink = 0;
+  let doEat = () => {eat++;};
+  let dodrink = () => {drink++;};
+  this.makeFeed = () => {doEat();};
+  this.makeDrink = () => {dodrink();};
+  this.say = () => {
+    console.log(`Hi I am ${this.name} and I am ${this.age}, wanna eat = ${eat}, wanna drink = ${drink}`);
+  }
+}
+
+let human = new Human('Igor', 29);
